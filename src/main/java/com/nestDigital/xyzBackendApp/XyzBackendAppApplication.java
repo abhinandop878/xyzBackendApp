db889166d619ad4ef588f6fec5c264102717024b
Abhinand Manohar OP <103516348+abhinandop878@users.noreply.github.com>
2022-10-19 09:15:40 +0530

    xyz backend app

---
+++ src/main/java/com/nestDigital/xyzBackendApp/XyzBackendAppApplication.java
@@ -0,0 +1,13 @@
+package com.nestDigital.xyzBackendApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XyzBackendAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XyzBackendAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nestDigital/xyzBackendApp/XyzBackendAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nestDigital.xyzBackendApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XyzBackendAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
